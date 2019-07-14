fe2f7f5e08b61606b9d4fa053bf6f8ba913fcb6d
olympia <owojcik7@gmail.com>
2019-07-14 12:43:01 -0400

    Add toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 //without .js bc our build workflow adds file extn
 import Aux from "../../hoc/Aux";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 //allows us to use layout component as a wrapper for core content we want to render to the screen
@@ -8,7 +9,8 @@ import classes from "./Layout.module.css";
 //add CSS class to main to make sure it has some margin
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDraw, Backdrop</div>
+    <Toolbar />
+    <div>SideDraw, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
