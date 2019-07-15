9795b69d3599aa166a68473b3714df563a00b650
olympia <owojcik7@gmail.com>
2019-07-14 21:23:11 -0400

    Improve performance through lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
-const OrderSummary = props => {
-  //get ingredients into the right order summary format
 
-  //I get ingredients in object format and want to map it out to list items
-  const ingredientSummary = Object.keys(props.ingredients).map(ingred => {
-    return (
-      <li key={ingred}>
-        <span style={{ textTransform: "capitalize" }}>{ingred} </span>:{" "}
-        {props.ingredients[ingred]}
-      </li>
+class OrderSummary extends Component {
+  //this could be a functional component, but we made it a class for lifecycle hooks
+
+  componentWillUpdate() {
+    console.log("OrderSummary will update");
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      ingred => {
+        return (
+          <li key={ingred}>
+            <span style={{ textTransform: "capitalize" }}>{ingred} </span>:{" "}
+            {this.props.ingredients[ingred]}
+          </li>
+        );
+      }
     );
-  });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={props.cancel} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={props.continue} btnType="Success">
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={this.props.cancel} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={this.props.continue} btnType="Success">
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
 //if modal is shown, the backdrop should be shown
-const Modal = props => {
-  return (
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </>
-  );
-};
+class Modal extends Component {
+  //only update if showChanges because that's the only thing which matters for me here
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
+  componentWillUpdate() {
+    console.log("Modal will update");
+  }
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
 export default Modal;
