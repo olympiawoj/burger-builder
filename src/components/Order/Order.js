6eee3a69261ef4e36252a3cfb93543ffd1f598b4
olympia <owojcik7@gmail.com>
2019-10-16 11:19:46 -0400

    Complete 257. Implementing Navigation Links

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./Order.module.css"
+
+//need to receive ingredients and price via props
+const order = (props) => {
+    <div className={classes.Order}>
+        <p>Ingredients: Salad ()</p>
+        <p>Price<strong>USD 5.45</strong></p>
+    </div>
+}
+
+export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react"
+import Order from "../../components/Order/Order"
 
-class Order extends Component {
-
+class Orders extends Component {
+    render() {
+        return (
+            //what do we wanna render? output Orders - create new component, the Order component itself
+            //orders neeeded are fetched from bacekend
+            <div>
+                <Order />
+                <Order />
+            </div>
+        )
+    }
 }
+export default Orders;
