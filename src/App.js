66b0432f5473f966d65cf12abc25c9c0adc6520f
olympia <owojcik7@gmail.com>
2019-11-19 18:08:39 -0500

    Persist auth state with localStorage

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders"
 import Auth from "./containers/Auth/Auth"
 import Logout from "./containers/Auth/Logout/Logout"
-
+import { connect } from "react-redux"
+import * as actions from "./store/actions/index"
 
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
   render() {
 
     return (
@@ -28,4 +33,10 @@ class App extends Component {
     );
   }
 }
-export default App;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+export default connect(null, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -29,6 +29,9 @@ export const authFail = (error) => {
 }
 //logout wil be a synchronous action creator, which will return an action type AUTH_LOGOUT I'll create, 
 export const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationDate")
+    localStorage.removeItem('userId')
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -66,6 +69,12 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
             .then((res) => {
                 console.log(res)
+                // * 1000 b/c JS time works in milliseconds & time we got was in seconds
+                //getTime turns into a time, then to turn into an object again wrap into newDate
+                const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000)
+                localStorage.setItem("token", res.data.idToken)
+                localStorage.setItem('expirationDate', expirationDate)
+                localStorage.setItem('userId', res.data.localId)
                 //pass on idToken and userId
                 dispatch(authSuccess(res.data.idToken, res.data.localId))
                 dispatch(checkAuthTimeout(res.data.expiresIn))
@@ -85,5 +94,28 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            dispatch(logout())
+        } else {
+            //what we retrieve from localStorage ia s tring, newDate converts into date
+            const expirationDate = new Date(localStorage.getItem("expirationDate"))
+            //use both pieces of info, dispatch authsuccess action creator b/c i know we're logged in, but only want to do this if we haven't passed expiry date
+            if (expirationDate > new Date()) {
+                dispatch(logout())
+            } else {
+                // if we're logged in, can get userId
+                const userId = localStorage.getItem('userId')
+                dispatch(authSuccess(token, userId))
+                //arg is amount of seconds until we should be logged, out, expiration date is the TIME we are logged out, not the amount of seconds
+                //Future date in seconds minus current date in seconds , diff is the expiry date
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
+            }
+        }
     }
 }
