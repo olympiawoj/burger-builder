0bed0795237e7dae028f808d31b563743442a9e0
olympia <owojcik7@gmail.com>
2019-12-06 09:47:31 -0500

    Test redux in auth.js reducer

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,34 @@
+import reducer from "./auth"
+import * as actionTypes from "../actions/actionTypes"
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: "/"
+        })
+    })
+    //execuete reducer functin with initial state and with an action where the type is actionTypes.AUTH_SUCCESS and now ofc pass the correct payload with that action here, not only type but payload
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: "/"
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: "/"
+        })
+    })
+})
